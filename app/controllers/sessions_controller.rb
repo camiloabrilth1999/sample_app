97a51a884d953dab22e4e73125207ee1da25b0f3
Camilo Obando <camiloabrilth1999@gmail.com>
2019-09-04 11:36:14 -0500

    Implemet advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,17 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
-      #flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      #flash[:danger] = 'Invalid email/password combination' # Not quite right! #El mensaje no se iba y se mostraba más de la cuenta.
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
